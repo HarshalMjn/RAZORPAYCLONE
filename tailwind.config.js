8dccdfe05c99c155b0c041d5df13d211b51e1bfe
HarhslaMjn <mahajanh006@gmail.com>
2023-03-16 21:39:24 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish", "sans-serif"],
+      
+      },
+      colors: {
+        deepBlue: "#02042a",
+        LightBlue300: "#4b94ed",
+        LightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        LightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+      }},
+    },
+    plugins: [],
+  }
+ 
